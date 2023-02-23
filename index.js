c298ba829fee5b4f2c2e4236ee3abedb48febf8e
Bart-Spons <bartspons31@gmail.com>
2023-02-23 15:26:33 +0100

    week3 back end

--- index.js
+++ index.js
@@ -2,6 +2,10 @@ const express  = require('express')
 const http = require("http");
 const app = express();
 
+//dit is voor form.ejs
+const bodyParser = require('body-parser');
+
+//openen via localhost/1900
 const host = "localhost";
 const port = 1900;
 
@@ -15,23 +19,37 @@ const people = [
   { name: 'Bob', age: 40 }
 ];
 
-// hello world test
+// hello world test, dit is de startpagina
 app.get("/", (req, res) => {
     res.send("Hello World!");
   });
 
-  // routing
+  // routing en people inladen
 app.get("/liked", (req, res) => {
     // res.render("liked.ejs", { data: port });
     res.render("liked.ejs", { people });
   });
 
+  //form  
+  app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get('/form', (req, res) => {
+  res.render('form.ejs');
+});
+
+app.post('/submit', (req, res) => {
+  const name = req.body.name;
+  const email = req.body.email;
+  
+  
+  res.send(`Name: ${name}, Email: ${email}`);
+});
   
 
-
+//de 404 pagina
   app.use((req, res, next) => {
     res.status(404).send("404");
   });
 
 
-app.listen(port, () => console.log(`listening to port: ${port}`));
+app.listen(port, () => console.log(`Ga naar de poort: ${port}`));
